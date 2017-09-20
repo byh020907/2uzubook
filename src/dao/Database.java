e25452b6d6cbd7b3cf13b501b3732e7401d6cfd2
yjh5424 <yjh54240@naver.com>
2017-09-20 22:01:03 +0900

    로그인 추가

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -12,25 +12,23 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import user.Account;
-import user.Award;
-import user.Circle;
-import user.Contest;
-import user.Intern;
+
+
 
 public class Database {
 	/*
-	 * �꽌踰� �궡�쓽 �뜲�씠�꽣踰좎씠�뒪瑜� �떎猷⑤뒗 �떛湲��넠 媛앹껜 
+	 * 占쎄퐣甕곤옙 占쎄땀占쎌벥 占쎈쑓占쎌뵠占쎄숲甕곗쥙�뵠占쎈뮞�몴占� 占쎈뼄�뙴�뫀�뮉 占쎈뼓疫뀐옙占쎈꽑 揶쏆빘猿� 
 	 */
 	
 	private Connection connection;
 	private static Database instance;
 	
     private Database() {
-    	// Driver 媛앹껜 �룞�쟻 濡쒕뵫 諛� connection �씤�뒪�꽩�뒪 珥덇린�솕
+    	// Driver 揶쏆빘猿� 占쎈짗占쎌읅 嚥≪뮆逾� 獄쏉옙 connection 占쎌뵥占쎈뮞占쎄쉘占쎈뮞 �룯�뜃由곤옙�넅
     	try {
-    		// 媛앹껜 �깮�꽦 �떆 �꽌踰꾩쓽 �뜲�씠�꽣踰좎씠�뒪�� �뿰寃� 
-    		Class.forName("com.mysql.jdbc.Driver");    // Driver �겢�옒�뒪瑜� �룞�쟻 濡쒕뵫 諛� �깮�꽦
-    		connection = DriverManager.getConnection("jdbc:mysql://10.156.145.112/", "root", "1022");    // �꽌踰� �궡�쓽 �뜲�씠�꽣踰좎씠�뒪���쓽 而ㅻ꽖�뀡 �깮�꽦
+    		// 揶쏆빘猿� 占쎄문占쎄쉐 占쎈뻻 占쎄퐣甕곌쑴�벥 占쎈쑓占쎌뵠占쎄숲甕곗쥙�뵠占쎈뮞占쏙옙 占쎈염野껓옙 
+    		Class.forName("com.mysql.jdbc.Driver");    // Driver 占쎄깻占쎌삋占쎈뮞�몴占� 占쎈짗占쎌읅 嚥≪뮆逾� 獄쏉옙 占쎄문占쎄쉐
+    		connection = DriverManager.getConnection("jdbc:mysql://10.156.145.112/", "root", "1022");    // 占쎄퐣甕곤옙 占쎄땀占쎌벥 占쎈쑓占쎌뵠占쎄숲甕곗쥙�뵠占쎈뮞占쏙옙占쎌벥 �뚣끇苑뽳옙�� 占쎄문占쎄쉐
     	} catch (ClassNotFoundException | SQLException e) {
     		e.printStackTrace();
     	} 
@@ -42,67 +40,27 @@ public class Database {
     	return instance;
     }
     
-    public JSONArray executeAndGet(String sql, Object ... objects) {
-    	// SELECT�� 媛숈씠 �뀒�씠釉붿쓽 媛믪쓣 議고쉶�븯�뒗 荑쇰━ �떎�뻾
-    	try {
-    		// connection PreparedStatement �떎�뻾
-    		PreparedStatement statement = connection.prepareStatement(sql);
-        	if (objects.length > 0) {
-        		// �뵆�젅�씠�뒪 ���뜑 (臾쇱쓬�몴 : ?) 媛� �떞湲� SQL 臾몄쓣 �떎�뻾 �떆 �뙆�씪誘명꽣 議고쉶
-    			int index = 1;
-    			for (Object object : objects) {
-        			statement.setObject(index++, object);
-        		}
-        		return filterData(statement.executeQuery());
-        	} else {
-        		// �뵆�젅�씠�뒪 ���뜑媛� �뾾�쓣 寃쎌슦 諛붾줈 SQL �떎�뻾
-        		return filterData(statement.executeQuery());
-        	} 
-    	} catch (SQLException sqlE) {
-    		sqlE.printStackTrace();
-    		return null;
-    	}
-    }
-    
-    public int executeAndUpdate(String sql, Object ... objects) {
-    	// DELETE, INSERT, UPDATE�� 媛숈씠 �뀒�씠釉붿쓽 媛믪쓣 蹂�寃쏀븯�뒗 荑쇰━ �떎�뻾
-    	try {
-    		PreparedStatement statement = connection.prepareStatement(sql);
-        	if (objects.length > 0) {
-    			int index = 1;
-    			for (Object object : objects) {
-        			statement.setObject(index++, object);
-        		}
-        		return statement.executeUpdate();
-        	} else {
-        		return statement.executeUpdate();
-        	} 
-    	} catch (SQLException sqlE) {
-    		sqlE.printStackTrace();
-    		return -1;
-    	}
-    }
-    
-    private static JSONArray filterData(ResultSet resultSet) throws SQLException {
-    	// �뒠�뵆�뱾�쓽 �뜲�씠�꽣媛� �떞湲� ResultSet 媛앹껜瑜� ArrayList �삎�깭濡� 媛�怨� 諛� 諛섑솚
-    	JSONArray results = new JSONArray();
-
-    	while (resultSet.next()) {
-    		// �뒠�뵆�뱾�쓽 紐⑸줉�쓣 議고쉶�븳�떎
-    		JSONObject result = new JSONObject();
-			ResultSetMetaData metaData = resultSet.getMetaData(); 
-			for (int i = 1; i <= metaData.getColumnCount(); i++) {
-				// �빐�떦 �뒠�뵆 �궡 硫뷀��뜲�씠�꽣瑜� 議고쉶�븳�떎
-    		    String label = metaData.getColumnLabel(i);
-    		    Object value = resultSet.getObject(i);
-    		    result.put(label, value);
-			}
-			results.add(result);    // results JSONArray�뿉 �뒠�뵆�쓽 �뜲�씠�꽣瑜� 異붽��븳�떎 
+	public int input_resume(Account account) {
+		String SQL = "insert into  values(?,?,?,?,?,?,?)";
+		
+		try {
+				PreparedStatement pstmt = connection.prepareStatement(SQL);
+				pstmt.setInt(1, account.getStudnet_id());
+				pstmt.setString(2, account.getId());
+				pstmt.setString(3, account.getPassword());
+				pstmt.setString(4, account.getIntro());
+				pstmt.setString(5, account.getName());
+				pstmt.setString(6, account.getGender());
+				pstmt.setString(7, account.getEmail());
+				return pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-    	
-    	return results;    // 鍮꾩젙�긽�쟻�씤 醫낅즺�씤吏�, 寃곌낵 媛믪씠 �뾾�뒗 寃껋씤吏� �솗�씤�븯湲� �쐞�빐 洹몃�濡� results 諛섑솚
-    }
-    
+		return -1;
+	}
+	
+	  
     public int login(String id,String password){
 		String sql="select password from account where id= ?";
 		
@@ -112,12 +70,12 @@ public class Database {
 			ResultSet rs=pstmt.executeQuery();
 			if(rs.next()){
 				if(rs.getString(1).equals(password)){
-					return 1; //�꽦怨�
+					return 1; //占쎄쉐�⑨옙
 				}else{
-					return 0; //濡쒓렇�씤 �떎�뙣(鍮꾨� 踰덊샇媛� �떎由�)
+					return 0; //嚥≪뮄�젃占쎌뵥 占쎈뼄占쎈솭(�뜮袁⑨옙 甕곕뜇�깈揶쏉옙 占쎈뼄�뵳占�)
 				}
 			}
-			return -1; //濡쒓렇�씤 �떎�뙣(�빐�떦 id 媛��뾾�쓬)
+			return -1; //嚥≪뮄�젃占쎌뵥 占쎈뼄占쎈솭(占쎈퉸占쎈뼣 id 揶쏉옙占쎈씨占쎌벉)
 			
 		}catch (Exception e) {
 			// TODO: handle exception
@@ -125,99 +83,10 @@ public class Database {
 		}
 		return 0;
 	}
+	
+	
     
-    public int join(Account account){
-		String sql="insert into account values(?,?,?,?,?,?,?)";
-		
-		try {
-			PreparedStatement pstmt=connection.prepareStatement(sql);
-			pstmt.setInt(1, account.getStudent_id());
-			pstmt.setString(2, account.getId());
-			pstmt.setString(3, account.getPassword());
-			pstmt.setString(4, account.getName());
-			pstmt.setString(5, account.getGender());
-			pstmt.setString(6, account.getMajor());
-			pstmt.setString(7, account.getEmail());
-			return pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return -1;//�뜲�씠�꽣踰좎씠�뒪 �삤瑜�
-	}
-    
-	public void inputCareer_award(ArrayList<Award> arrayList, int student_id) {
-		String sql = "insert into career_award values(?,?,?,?)";
-
-		
-		try {
-			PreparedStatement pstmt = connection.prepareStatement(sql);
-
-			for (int i = 0; i < arrayList.size(); i++) {
-				
-				pstmt.setInt(1, arrayList.get(i).getStudent_id());
-				pstmt.setString(2, arrayList.get(i).getAward());
-				pstmt.setString(3, arrayList.get(i).getAwardName());
-				pstmt.setString(4, arrayList.get(i).getAwardDate());
-				pstmt.addBatch();
-				System.out.println("성공");
-			}	
-			 pstmt.executeBatch();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-    }
-    
- 
-    
-    public int getNextNum(){
-		String SQL="SELECT USERNO FROM serial ORDER BY USERNO DESC limit 1";
-		try{
-			PreparedStatement pstmt=connection.prepareStatement(SQL);
-			ResultSet rs=pstmt.executeQuery();
-			if(rs.next()){
-				return rs.getInt(1)+1;
-			}
-			return 1;//泥� 踰덉㎏ 寃뚯떆臾쇱씤 寃쎌슦
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		return -1;//�뜲�씠�꽣踰좎씠�뒪 �삤瑜�
-		
-	}
-    
-    public int createSerialKey(){
-    	String sql="INSULT INTO serial VALUES(?,?,?)";
-		
-    	int num=0;
-    	String serialKey="";
-    	String content="";
-		try{
-			PreparedStatement pstmt=connection.prepareStatement(sql);
-			pstmt.setInt(1, num);
-			pstmt.setString(2, serialKey);
-			pstmt.setString(3, content);
-			
-			return pstmt.executeUpdate();
-			
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-    	return -1;//�뜲�씠�꽣踰좎씠�뒪 �삤瑜�
-    }
-    
-    public JSONArray search(String name, int student_id, String major){
-    	String SQL="select a.student_id, a.name, a.gender, a.major from account as a "
-    			+ "where a.name like ?% or a.student_id like ?% or a.major like ?%";
-    	try{
-			JSONArray arr=executeAndGet(SQL,name,student_id,major);
-			return arr;
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		return null;//데이터베이스 오류
-    }
-    
+  
  
 }
 
