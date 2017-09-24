9157dd153dc11631e33daa4eb005ef2b1fb16dbf
yjh5424 <yjh54240@naver.com>
2017-09-24 22:35:34 +0900

    함수 명세화 추가

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -89,7 +89,6 @@ public class Database {
 		String SQL = "insert into resume values(?,?,?,?,?,?,?)";
 		String NULL_FINISHDATE_SQL = "insert into resume"
 				+ "(student_id,name,content,startDate,resume_category,keyword_category) " + "values(?,?,?,?,?,?)";
-
 		try {
 			if (check == 0) {
 				PreparedStatement pstmt = connection.prepareStatement(SQL);
@@ -111,12 +110,12 @@ public class Database {
 				pstmt.setInt(6, resume.getKeyword_category());
 				return pstmt.executeUpdate();
 			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return 0;
     }
+	
 	
     
   
