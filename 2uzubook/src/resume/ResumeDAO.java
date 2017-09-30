d55da84ee43b48d166d76e73c1018a6d1f4631b2
yjh5424 <yjh54240@naver.com>
2017-09-30 14:34:29 +0900

    프로젝트 추가

--- 2uzubook/src/resume/ResumeDAO.java
+++ 2uzubook/src/resume/ResumeDAO.java
@@ -146,6 +146,8 @@ public class ResumeDAO {
 		return -1;
 	}
 	
+	
+	//모든 컬럼을 다입력하는 클럽
 	public int inert_club(Club club) {
 		String SQL="insert into club (user,name,`desc`,startdate,enddate,keyword) values (?,?,?,?,?,?)";
 		
@@ -154,11 +156,57 @@ public class ResumeDAO {
 		return executeAndUpdate(SQL, club.getUser(),club.getName(),club.getDesc(),club.getStartDate()
 				,club.getEndDate(),club.getKeyword());
 		// 성공이면 0 이상
-	}catch (Exception e) {
-		// TODO: handle exception
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return -1;
 	}
-	
-	return -1;
+
+	// 끝나는 시간 입력안하는 함수
+	public int inert_club_remove_enddate(Club club) {
+		String SQL = "insert into club (user,name,`desc`,startdate,keyword) values (?,?,?,?,?)";
+
+		try {
+			return executeAndUpdate(SQL, club.getUser(), club.getName(), club.getDesc(), club.getStartDate(),
+					club.getKeyword());
+			// 성공이면 0 이상
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return -1;
 	}
+
+	// 모든 컬럼을 다입력하는 클럽
+	public int inert_project(Project project) {
+		String SQL = "insert into project (user,name,`desc`,startdate,enddate,keyword) values (?,?,?,?,?,?)";
+
+		try {
+			return executeAndUpdate(SQL, project.getUser(), project.getName(), project.getDesc(),
+					project.getStartDate(), project.getEndDate(), project.getKeyword());
+			// 성공이면 0 이상
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return -1;
+	}
+
+	// 끝나는 시간 입력안하는 함수
+	public int inert_project_remove_enddate(Project project) {
+		String SQL = "insert into project (user,name,`desc`,startdate,keyword) values (?,?,?,?,?)";
+
+		try {
+			return executeAndUpdate(SQL, project.getUser(), project.getName(), project.getDesc(), project.getStartDate(),
+					project.getKeyword());
+			// 성공이면 0 이상
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return -1;
+	}
+		
 	
 }
