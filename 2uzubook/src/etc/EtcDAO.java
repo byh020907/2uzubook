730fb6eb03ceda7f34c15f04c06e3747a9e15b7b
yjh5424 <yjh54240@naver.com>
2017-10-02 01:04:13 +0900

    독서 봉사 업데이트 문 추가

--- 2uzubook/src/etc/EtcDAO.java
+++ 2uzubook/src/etc/EtcDAO.java
@@ -31,5 +31,31 @@ public class EtcDAO extends Database {
 
 		return -1;
 	}
+	
+	public int update_reading(String name,String date,int keyword) {
+		String SQL="update reading set name=?,date=?,keyword=?";
+		
+		try {
+			return executeAndUpdate(SQL,name,date,keyword);
+			// 성공이면 0 이상
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return -1;
+	}
+	
+	public int update_volunteer(String name,String ins,String startTime,String endTime,int keyword) {
+	String SQL="update volunteer set name=?,ins=?,startTime=?,endTime=?,keyword=?";
+		
+		try {
+			return executeAndUpdate(SQL,name,ins,startTime,endTime,keyword);
+			// 성공이면 0 이상
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return -1;
+	}
 
 }
