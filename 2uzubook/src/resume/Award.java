da6d6530cc08a4048e727f3348d8093d31622b47
yjh5424 <yjh54240@naver.com>
2017-10-03 22:57:22 +0900

    award 클래스 수정

--- 2uzubook/src/resume/Award.java
+++ 2uzubook/src/resume/Award.java
@@ -2,38 +2,53 @@ package resume;
 
 public class Award {
 	private int id; // db 에서 구별하는 번호임
-	private String user; // user id 
+	private String user; // user id
 	private String name; // title
 	private String ins; // 주최측 정보
+	private String grade;
 	private String date;
 	private int keyword;
-	
-	public Award(String user, String name, String ins, String date, int keyword) {
+
+	public Award(String user, String name, String ins, String grade, String date, int keyword) {
 		super();
 		this.user = user;
 		this.name = name;
 		this.ins = ins;
+		this.grade = grade;
 		this.date = date;
 		this.keyword = keyword;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUser() {
 		return user;
 	}
+
 	public void setUser(String user) {
 		this.user = user;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
 	}
 	public String getIns() {
 		return ins;
