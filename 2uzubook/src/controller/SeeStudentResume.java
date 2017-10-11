620a13b92b5d6156adb4a960f3a30c5a87b1c76e
BAE Yong Ho <byh020907@naver.com>
2017-10-11 19:59:43 +0900

    인덱스페이지에서 학생클릭시 그항생 페이지로 가도록 변경

---
+++ 2uzubook/src/controller/SeeStudentResume.java
@@ -0,0 +1,82 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import etc.EtcDAO;
+import resume.ResumeDAO;
+import user.UserDAO;
+
+/**
+ * Servlet implementation class myresume
+ */
+@WebServlet("/SeeStudentResume")
+public class SeeStudentResume extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	ResumeDAO database;
+	EtcDAO database2;
+	UserDAO database3;
+	
+    public SeeStudentResume() {
+    	database=ResumeDAO.getInstance();
+    	database2=EtcDAO.getInstance();
+    	database3=UserDAO.getInstance();
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Util.setCharset(request, response, "utf-8");
+		
+		HttpSession session=request.getSession();
+		
+		String companySerial=(String) session.getAttribute("serialKey");
+		if(session.getAttribute("serialKey")==null){
+			PrintWriter out=response.getWriter();
+			out.print("<script>alert('회사 로그인을 먼저하세요.')</script>");
+			out.flush();
+			response.sendRedirect("/2uzubook/login.html");
+			return;
+		}
+		
+		String userID=request.getParameter("id");
+		
+		JSONArray licenses=database.select_resume(userID, 1);
+		JSONArray awds=database.select_resume(userID, 2);
+		JSONArray clubs=database.select_resume(userID, 3); 
+		JSONArray projects=database.select_resume(userID, 4);
+		JSONArray tests=database.select_resume(userID, 5);
+		JSONArray conferences=database.select_resume(userID, 6);
+		JSONArray volunteers=database2.select_resume(userID, 1);
+		JSONArray readings=database2.select_resume(userID, 2);
+		
+		//유저 정보 받아오기
+		JSONArray ja=database3.executeAndGet("SELECT * FROM USER WHERE id=?", userID);
+
+		JSONObject userData=(JSONObject)ja.get(0);
+		
+		int major=(Integer)userData.get("major");
+		
+		userData.put("majorName", database3.majorToString(major));
+		
+		userData.put("licenses", licenses);
+		userData.put("awards",awds);
+		userData.put("clubs",clubs);
+		userData.put("projects",projects);
+		userData.put("tests",tests);
+		userData.put("conferences",conferences);
+		userData.put("volunteers",volunteers);
+		userData.put("readings",readings);
+		request.setAttribute("JSONObject", userData);
+		request.getRequestDispatcher("/myresume.jsp").forward(request, response);
+	}
+
+}
