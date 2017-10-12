d2cf305b172ddd0b02de120eb0f34ee511a82355
yjh5424 <yjh54240@naver.com>
2017-10-12 14:36:40 +0900

    update 추가

--- 2uzubook/src/user/User.java
+++ 2uzubook/src/user/User.java
@@ -11,6 +11,50 @@ public class User {
 	private char gender;
 	private String major;
 	private String email;
+	private String profile_image_path;
+	private String main_image_path;
+	private String github_address;
+	private String facebook_address;
+	
+	public User(String profile_image_path, String main_image_path, String github_address, String facebook_address) {
+		super();
+		this.profile_image_path = profile_image_path;
+		this.main_image_path = main_image_path;
+		this.github_address = github_address;
+		this.facebook_address = facebook_address;
+	}
+
+	public String getProfile_image_path() {
+		return profile_image_path;
+	}
+
+	public void setProfile_image_path(String profile_image_path) {
+		this.profile_image_path = profile_image_path;
+	}
+
+	public String getMain_image_path() {
+		return main_image_path;
+	}
+
+	public void setMain_image_path(String main_image_path) {
+		this.main_image_path = main_image_path;
+	}
+
+	public String getGithub_address() {
+		return github_address;
+	}
+
+	public void setGithub_address(String github_address) {
+		this.github_address = github_address;
+	}
+
+	public String getFacebook_address() {
+		return facebook_address;
+	}
+
+	public void setFacebook_address(String facebook_address) {
+		this.facebook_address = facebook_address;
+	}
 
 	public int getAdm_year() {
 		return adm_year;
